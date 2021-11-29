d0e7d1bf49bbce91148955f4530fb3570e1de2d3
Ryan.K <ryankung@ieee.org>
2021-11-29 19:20:22 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
+import os
+from setuptools import setup, find_packages, Extension
 
-from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = [
+    line
+    for line in open(
+        os.path.join(here, "requirements.txt"),
+        "r"
+    )
+]
 
 setup(name='EtherSwarm',
       version='0.1',
       description='Manage your ether addresses',
       author='Elder Ryan',
+      license='GPL',
       author_email='ryankung@ieee.org',
       packages=['etherswarm'],
+      url='https://github.com/ZeroProphet/etherswarm',
+      install_requires=install_requires,
+      entry_points={
+          "console_scripts": [
+              "etherswarm=etherswarm:main"
+          ]
+      }
      )
