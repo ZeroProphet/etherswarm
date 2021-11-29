59ea60126de465d331c008c24552fe49eaab4e7d
Ryan.K <ryankung@ieee.org>
2021-11-29 19:08:27 +0800

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EtherSwarm',
+      version='0.1',
+      description='Manage your ether addresses',
+      author='Elder Ryan',
+      author_email='ryankung@ieee.org',
+      packages=['etherswarm'],
+     )
